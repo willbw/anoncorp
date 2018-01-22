8515c4fababa33b3b95b06b568fb7cfc40d998e8
Will <willwright90@gmail.com>
2018-01-22 19:30:55 +1100

    second prep for heroku

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/api/customerlist', (req, res) => {
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
